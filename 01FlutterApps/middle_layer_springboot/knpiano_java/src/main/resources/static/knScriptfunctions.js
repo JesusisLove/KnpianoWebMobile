cf4dc570631d13c9dda58e68b54fca23cdda59c5
JesusisLove <liu_ym@hotmail.com>
2024-05-20 12:56:36 +0900

    2024/05/20 后台开发:整理代码

---
+++ 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/resources/static/knScriptfunctions.js
@@ -0,0 +1,17 @@
+// knScriptfunctions.js
+// function getButtonStates(info) {
+//     let states = {
+//         change: '',
+//         delete: '',
+//         sign: '',
+//         cancel: ''
+//     };
+
+//     if (info.lessonType == 0 && info.schedualDate && !info.lsnAdjustedDate && !info.scanQRDate) {
+//         states.change = '';
+//         states.delete = '';
+//         states.sign = '';
+//         states.cancel = 'disabled';
+//     }
+//     return states;
+// }
