b08feb1dea48c25fc0734c78cf231875cd572c71
JesusisLove <liu_ym@hotmail.com>
2024-06-05 22:54:45 +0900

    2024/06/05 后台维护开发：学科枝番信息管理模块功能追加 1.学科枝番信息一览，检索 2.学科枝番信息新规，变更，删除

---
+++ 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/mapper/Kn05S003SubjectEdabnMapper.java
@@ -0,0 +1,27 @@
+package com.liu.springboot04web.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.liu.springboot04web.bean.Kn05S003SubjectEdabnBean;
+public interface Kn05S003SubjectEdabnMapper  {
+
+    public List<Kn05S003SubjectEdabnBean> getInfoList();
+    public Kn05S003SubjectEdabnBean getInfoById(@Param("subId")String subId, 
+                                                @Param("edabanId") String edabanId);
+    // 根据特定条模糊检索学生档案信息
+    public List<Kn05S003SubjectEdabnBean> searchEdaSubject(@Param("params") Map<String, Object> queryparams);
+                                                
+    public void updateInfo(Kn05S003SubjectEdabnBean bean);
+    public void insertInfo(Kn05S003SubjectEdabnBean bean);
+    public void deleteInfo(@Param("subId")String subId, 
+                           @Param("edabanId") String edabanId);
+
+    // 执行数据库存储过程
+    public void callProcedure(Map<String, Integer> map);
+    // 获取下一个序列值
+    public void getNextSequence(Map<String, Object> map);
+
+}
