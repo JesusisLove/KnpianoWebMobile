80e88510fa2626fbb11e70a9de4364369783287d
JesusisLove <liu_ym@hotmail.com>
2024-06-07 08:55:45 +0900

    2024/06/07 后台维护开发：学生银行账户信息管理模块功能追加 1.学生银行账户信息管理，检索 2.学生银行账户信息管理新规，删除

--- 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/dao/Kn05S002StubnkDao.java
+++ 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/dao/Kn05S002StubnkDao.java
@@ -1,15 +1,13 @@
 package com.liu.springboot04web.dao;
 
 import com.liu.springboot04web.bean.Kn05S002StubnkBean;
-import com.liu.springboot04web.constant.KNConstant;
 import com.liu.springboot04web.mapper.Kn05S002StubnkMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;@Repository
+@Repository
 public class Kn05S002StubnkDao implements InterfaceKnPianoDao {
 
     @Autowired
@@ -17,20 +15,20 @@ public class Kn05S002StubnkDao implements InterfaceKnPianoDao {
 
     public List<Kn05S002StubnkBean> getInfoList() {
         List<Kn05S002StubnkBean> list =kn05S002StubnkMapper.getInfoList();
-        System.out.println("selectの学生銀行番号管理データ：" + list.toString());
         return list;
     }
 
-
     public Kn05S002StubnkBean getInfoById(String id) {
-        Kn05S002StubnkBean kn05S002StubnkBean = kn05S002StubnkMapper.getInfoById(id);
-        System.out.println("selectの学生銀行番号管理データ：" + kn05S002StubnkBean.toString());
+        return null;
+    }
+
+    public Kn05S002StubnkBean getInfoById(String stuId, String bankId) {
+        Kn05S002StubnkBean kn05S002StubnkBean = kn05S002StubnkMapper.getInfoById(stuId, bankId);
         return kn05S002StubnkBean;
     }
 
     public void save(Kn05S002StubnkBean kn05S002StubnkBean) {
-        if ((kn05S002StubnkBean.getBankId() == null) || (kn05S002StubnkBean.getBankId().isEmpty()) && 
-            (kn05S002StubnkBean.getStuId() == null)|| (kn05S002StubnkBean.getStuId().isEmpty())) { 
+        if (getInfoById(kn05S002StubnkBean.getStuId(), kn05S002StubnkBean.getBankId()) == null) { 
 
             insert(kn05S002StubnkBean);
         } else {
@@ -38,25 +36,19 @@ public class Kn05S002StubnkDao implements InterfaceKnPianoDao {
         }
     }
 
-
     // 新規
     private void insert(Kn05S002StubnkBean kn05S002StubnkBean) {
-        System.out.println("insertの学生銀行番号管理データ：" + kn05S002StubnkBean.toString());
         kn05S002StubnkMapper.insertInfo(kn05S002StubnkBean);
 
     }
 
-
     // 変更
     private void update(Kn05S002StubnkBean kn05S002StubnkBean) {
-        System.out.println("updateの学生銀行番号管理データ：" + kn05S002StubnkBean.toString());
         kn05S002StubnkMapper.updateInfo(kn05S002StubnkBean);
     }
 
-
     // 削除
     public void delete(String id) { 
-        System.out.println("deleteの学生銀行番号管理データ：keiyaku_mng_no = " + id);
         kn05S002StubnkMapper.deleteInfo(id); 
     }
 }
