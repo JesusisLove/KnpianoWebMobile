63fb4d170aaa917f8af9d5734384c0e61d3be1e0
JesusisLove <liu_ym@hotmail.com>
2024-06-14 18:45:50 +0900

    2024/06/14 后台维护:代码整理,删除掉没有使用的代码

--- 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/controller/Kn02F004UnpaidController.java
+++ 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/controller/Kn02F004UnpaidController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import com.liu.springboot04web.bean.Kn02F002FeeBean;
 import com.liu.springboot04web.bean.Kn02F004UnpaidBean;
 import com.liu.springboot04web.bean.Kn05S002StubnkBean;
 import com.liu.springboot04web.dao.Kn02F004UnpaidDao;
