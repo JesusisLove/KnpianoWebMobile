2721eacf54699b77685e27a71ad131576f7d0374
JesusisLove <liu_ym@hotmail.com>
2024-06-22 18:52:05 +0900

    2024/06/22 后台维护开发：为前端手机的科目级别模块添加只为指定科目的科目级别执行查询的处理 1.科目管理模块：科目级别一览

--- 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/controller_mobile/Kn05S003SubEdaController4Mobile.java
+++ 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/controller_mobile/Kn05S003SubEdaController4Mobile.java
@@ -1,5 +1,6 @@
 package com.liu.springboot04web.controller_mobile;
 
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,42 +15,41 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
-import com.liu.springboot04web.bean.Kn01B002SubBean;
-import com.liu.springboot04web.dao.Kn01B002SubDao;
+import com.liu.springboot04web.bean.Kn05S003SubjectEdabnBean;
+import com.liu.springboot04web.dao.Kn05S003SubEdaBanDao;
 
 @RestController
-public class Kn01L001SubController4Mobile {
+public class Kn05S003SubEdaController4Mobile {
 
     @Autowired
-    Kn01B002SubDao knSub001Dao;
+    Kn05S003SubEdaBanDao kn05S003SubjectEdabnDao;
 
     @CrossOrigin(origins = "*") // 它允许接受来自所有的请求，不安全，生产环境中严谨使用“*”设置。
-    @GetMapping("/mb_kn_sub_001_all")
-    public ResponseEntity<Collection<Kn01B002SubBean>> getSubdentList() {
+    @GetMapping("/mb_kn_05s003_subject_edabn_by_subid/{subId}")
+    public ResponseEntity<Collection<Kn05S003SubjectEdabnBean>> getSubdentList(@PathVariable("subId") String subId) {
         // 获取目前所有科目信息显示在画面一览上
-        Collection<Kn01B002SubBean> collection = knSub001Dao.getInfoList();
+        Collection<Kn05S003SubjectEdabnBean> collection = kn05S003SubjectEdabnDao.getSubEdaList(subId);
         return ResponseEntity.ok(collection);
     }
 
     // 【新規/编辑】画面にて、【保存】ボタンを押下
     @CrossOrigin(origins = "*") 
-    @PostMapping("/mb_kn_sub_001")
-    public void excuteInfoAdd(@RequestBody Kn01B002SubBean knSub001Bean) {
-        knSub001Dao.save(knSub001Bean);
+    @PostMapping("/mb_kn_05s003_subject_edabn")
+    public void excuteInfoAdd(@RequestBody Kn05S003SubjectEdabnBean knSub001Bean) {
+        kn05S003SubjectEdabnDao.save(knSub001Bean);
     }
 
     // 【学科一覧】削除ボタンを押下
     @CrossOrigin(origins = "*") 
-    @DeleteMapping("/mb_kn_sub_001/{id}")
-    public ResponseEntity<String> executeInfoDelete(@PathVariable("id") String id) {
+    @DeleteMapping("/mb_kn_05s003_subject_edabn/{subId}/{edabanId}")
+    public ResponseEntity<String> executeInfoDelete(@PathVariable("subId") String subId,
+                                                    @PathVariable("edabanId") String edabanId) {
         try {
-            knSub001Dao.delete(id);
+            kn05S003SubjectEdabnDao.delete(subId, edabanId);
             return ResponseEntity.ok("success");
         } catch (DataIntegrityViolationException e) {
             return ResponseEntity.status(HttpStatus.CONFLICT)  // 409 Conflict
-                    .body("学科编号为【" + id + "】的科目在使用。所以删除不了。");
+                    .body("学科级别编号为【" + edabanId + "】的科目有学生在用。所以删除不了。");
         }
     }
-    
 }
-
--- 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/mapper/Kn05S003SubEdaBanMapper.java
+++ 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/mapper/Kn05S003SubEdaBanMapper.java
@@ -8,9 +8,15 @@ import org.apache.ibatis.annotations.Param;
 import com.liu.springboot04web.bean.Kn05S003SubjectEdabnBean;
 public interface Kn05S003SubEdaBanMapper  {
 
+    // 后台维护查询用
     public List<Kn05S003SubjectEdabnBean> getInfoList();
+    
+    // 手机端查询用
+    public List<Kn05S003SubjectEdabnBean> getSubEdaList(@Param("subId")String subId);
+
     public Kn05S003SubjectEdabnBean getInfoById(@Param("subId")String subId, 
                                                 @Param("edabanId") String edabanId);
+
     // 根据特定条模糊检索学生档案信息
     public List<Kn05S003SubjectEdabnBean> searchEdaSubject(@Param("params") Map<String, Object> queryparams);
                                                 
