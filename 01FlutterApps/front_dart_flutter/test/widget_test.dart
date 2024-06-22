2b1882a09b13fbb86dacf9db9d9333abf800bb19
JesusisLove <liu_ym@hotmail.com>
2024-06-22 22:38:31 +0900

    2024/06/22 前端开发:代码调整

--- 01FlutterApps/front_dart_flutter/test/widget_test.dart
+++ 01FlutterApps/front_dart_flutter/test/widget_test.dart
@@ -9,12 +9,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kn_piano/HomePage.dart';
 
-import 'package:kn_piano/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(HomePage());
+    await tester.pumpWidget(const HomePage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
