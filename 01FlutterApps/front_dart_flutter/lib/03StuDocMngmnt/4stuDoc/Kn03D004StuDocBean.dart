fdeab1f1f903a9c4688abbae93957334291ff804
JesusisLove <liu_ym@hotmail.com>
2024-06-28 22:46:11 +0900

    2024/06/28 手机前端开发：学生档案模块(编辑，删除) 1.完善学生科目详细一览画面：编辑，删除功能实现。 2.解决前端从后端java拿过来的日期值，总是比后端向后偏差1天的处理。 	（例如后端给的日期是2424-01-01，到了前端就变成了2023-12-31T15:00:00.000+00:00）

--- 01FlutterApps/front_dart_flutter/lib/03StuDocMngmnt/4stuDoc/Kn03D004StuDocBean.dart
+++ 01FlutterApps/front_dart_flutter/lib/03StuDocMngmnt/4stuDoc/Kn03D004StuDocBean.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 class Kn03D004StuDocBean {
   final String stuId;
   final String subjectId;
@@ -6,7 +8,6 @@ class Kn03D004StuDocBean {
   final String subjectName;
   final String subjectSubName; // 枝番名称
   final String adjustedDate;
-  // late DateTime adjustedDate;
   final int    payStyle;
   final int    minutesPerLsn;
   final double lessonFee;
@@ -32,6 +33,20 @@ class Kn03D004StuDocBean {
 
   // 从后台拿到的json数据转化为Kn03D004StuDocBean对象
   factory Kn03D004StuDocBean.fromJson(Map<String, dynamic> json) {
+    String formattedAdjustedDate = '';
+    try {
+      if (json['adjustedDate'] != null && json['adjustedDate'] != '') {
+        DateTime parsedDate = DateTime.parse(json['adjustedDate']);
+        /* 解决从后端java传到前端，日期总是偏差一天的处理 例如，java端给出的日期是2024-01-01，到了前端却变成了2023-12-31T15:00:00.000+00:00 
+        下面代码是处理从后端java传递过来的日期，在前端也能正常显示
+        */
+        formattedAdjustedDate = DateFormat('yyyy-MM-dd').format(parsedDate.toLocal());
+      }
+    } catch (e) {
+      // ignore: avoid_print
+      print('Error parsing or formatting adjustedDate: $e');
+    }
+
     return Kn03D004StuDocBean(
       stuId:              json['stuId'] ?? '',
       subjectId:          json['subjectId'] ?? '',
@@ -39,8 +54,7 @@ class Kn03D004StuDocBean {
       stuName:            json['stuName'] ?? '',
       subjectName:        json['subjectName'] ?? '',
       subjectSubName:     json['subjectSubName'] ?? '',
-      // adjustedDate:       DateTime.parse(json['adjustedDate']).toLocal(),
-      adjustedDate:       json['adjustedDate'] ?? '',
+      adjustedDate:       formattedAdjustedDate,
       payStyle:           json['payStyle'] ?? 0,
       minutesPerLsn:      json['minutesPerLsn'] ?? 0,
       lessonFee:          json['lessonFee']?.toDouble() ?? 0.0,
--- 01FlutterApps/front_dart_flutter/lib/03StuDocMngmnt/4stuDoc/kn03D004StuDoc_Edit.dart
+++ 01FlutterApps/front_dart_flutter/lib/03StuDocMngmnt/4stuDoc/kn03D004StuDoc_Edit.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: use_build_context_synchronously, use_super_parameters, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -15,23 +15,25 @@ class StudentDocumentEditPage extends StatefulWidget {
   final String adjustedDate;
 
   const StudentDocumentEditPage({
-    super.key,
+    Key? key,
     required this.stuId,
     required this.subjectId,
     required this.subjectSubId,
     required this.adjustedDate,
-  });
+  }) : super(key: key);
 
   @override
   _StudentDocumentEditPageState createState() => _StudentDocumentEditPageState();
 }
 
 class _StudentDocumentEditPageState extends State<StudentDocumentEditPage> {
+  late Future<void> _initialData;
   String stuName = '';
   String subjectName = '';
   String subjectSubName = '';
-  List<DurationBean>? duration = [];
+  List<DurationBean> duration = []; // 初始化为空列表
   bool isMonthlyPayment = true;
+  int? payStyle;
   int? selectedDuration;
   double standardPrice = 0.0;
   TextEditingController adjustedPriceController = TextEditingController();
@@ -39,12 +41,18 @@ class _StudentDocumentEditPageState extends State<StudentDocumentEditPage> {
   @override
   void initState() {
     super.initState();
-    fetchStudentInfo();
-    fetchDurations();
+    selectedDuration = null; // 确保初始化为 null
+    _initialData = _fetchInitialData();
+  }
+
+  Future<void> _fetchInitialData() async {
+    await fetchStudentInfo();
+    await fetchDurations();
   }
 
   Future<void> fetchStudentInfo() async {
-    final String apiStuDocUrl = '${KnConfig.apiBaseUrl}${Constants.stuDocInfoEdit}/${widget.stuId}/${widget.subjectId}/${widget.subjectSubId}/${widget.adjustedDate}';
+    final String apiStuDocUrl =
+        '${KnConfig.apiBaseUrl}${Constants.stuDocInfoEdit}/${widget.stuId}/${widget.subjectId}/${widget.subjectSubId}/${widget.adjustedDate}';
     final response = await http.get(Uri.parse(apiStuDocUrl));
 
     if (response.statusCode == 200) {
@@ -54,7 +62,11 @@ class _StudentDocumentEditPageState extends State<StudentDocumentEditPage> {
         stuName = studentInfo['stuName'] ?? '';
         subjectName = studentInfo['subjectName'] ?? '';
         subjectSubName = studentInfo['subjectSubName'] ?? '';
-        standardPrice = studentInfo['standardPrice'] ?? 0.0;
+        selectedDuration = studentInfo['minutesPerLsn'] ?? 0;
+        standardPrice = studentInfo['lessonFee'] ?? 0.0;
+        adjustedPriceController.text =
+            (studentInfo['lessonFeeAdjusted'] ?? 0.0).toStringAsFixed(2);
+        payStyle = studentInfo['payStyle'];
       });
     } else {
       throw Exception('Failed to load student info');
@@ -62,8 +74,8 @@ class _StudentDocumentEditPageState extends State<StudentDocumentEditPage> {
   }
 
   Future<void> fetchDurations() async {
-    final String apiLsnDruationUrl = '${KnConfig.apiBaseUrl}${Constants.stuDocLsnDuration}';
-    final responseDuration = await http.get(Uri.parse(apiLsnDruationUrl));
+    final String apiLsnDurationUrl = '${KnConfig.apiBaseUrl}${Constants.stuDocLsnDuration}';
+    final responseDuration = await http.get(Uri.parse(apiLsnDurationUrl));
 
     if (responseDuration.statusCode == 200) {
       final decodedBody = utf8.decode(responseDuration.bodyBytes);
@@ -80,120 +92,134 @@ class _StudentDocumentEditPageState extends State<StudentDocumentEditPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('学生档案编辑')),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextFormField(
-              initialValue: stuName,
-              readOnly: true,
-              decoration: const InputDecoration(
-                labelText: '学生姓名',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              initialValue: subjectName,
-              readOnly: true,
-              decoration: const InputDecoration(
-                labelText: '科目名称',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              initialValue: subjectSubName,
-              readOnly: true,
-              decoration: const InputDecoration(
-                labelText: '科目级别名称',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              initialValue: widget.adjustedDate,
-              readOnly: true,
-              decoration: const InputDecoration(
-                labelText: '调整日付',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 8),
-            const Text('科目支付区分'),
-            Row(
-              children: [
-                Expanded(
-                  child: RadioListTile<bool>(
-                    title: const Text('按月付费'),
-                    value: true,
-                    groupValue: isMonthlyPayment,
-                    onChanged: (bool? value) {
+      body: FutureBuilder<void>(
+        future: _initialData,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    initialValue: stuName,
+                    readOnly: true,
+                    decoration: const InputDecoration(
+                      labelText: '学生姓名',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    initialValue: subjectName,
+                    readOnly: true,
+                    decoration: const InputDecoration(
+                      labelText: '科目名称',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    initialValue: subjectSubName,
+                    readOnly: true,
+                    decoration: const InputDecoration(
+                      labelText: '科目级别名称',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    initialValue: widget.adjustedDate,
+                    readOnly: true,
+                    decoration: const InputDecoration(
+                      labelText: '调整日期',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  const Text('科目支付方式'),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: RadioListTile<bool>(
+                          title: const Text('按月付费'),
+                          value: (payStyle == 1),
+                          groupValue: isMonthlyPayment,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              payStyle = 1;
+                            });
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: RadioListTile<bool>(
+                          title: const Text('课时付费'),
+                          value: (payStyle == 0),
+                          groupValue: isMonthlyPayment,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              payStyle = 0;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+
+                  DropdownButtonFormField<int>(
+                    decoration: const InputDecoration(
+                      labelText: '课程时长',
+                      border: OutlineInputBorder(),
+                    ),
+                    value: selectedDuration,
+                    items: duration.map((DurationBean durationBean) {
+                      return DropdownMenuItem<int>(
+                        value: durationBean.minutesPerLsn,
+                        child: Text('${durationBean.minutesPerLsn} 分钟'),
+                      );
+                    }).toList(),
+                    onChanged: (int? newValue) {
                       setState(() {
-                        isMonthlyPayment = value ?? true;
+                        selectedDuration = newValue;
                       });
                     },
                   ),
-                ),
-                Expanded(
-                  child: RadioListTile<bool>(
-                    title: const Text('课时付费'),
-                    value: false,
-                    groupValue: isMonthlyPayment,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        isMonthlyPayment = value ?? false;
-                      });
-                    },
+                  const SizedBox(height: 8),
+
+                  TextFormField(
+                    initialValue: standardPrice.toStringAsFixed(2),
+                    readOnly: true,
+                    decoration: const InputDecoration(
+                      labelText: '标准价格',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 8),
-            DropdownButtonFormField<int>(
-              decoration: const InputDecoration(
-                labelText: '课程时长',
-                border: OutlineInputBorder(),
+                  const SizedBox(height: 8),
+
+                  TextFormField(
+                    controller: adjustedPriceController,
+                    decoration: const InputDecoration(
+                      labelText: '调整价格',
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: saveData,
+                    child: const Text('保存'),
+                  ),
+                ],
               ),
-              value: selectedDuration,
-              items: duration?.map((DurationBean durationBean) {
-                return DropdownMenuItem<int>(
-                  value: durationBean.minutesPerLsn,
-                  child: Text('${durationBean.minutesPerLsn} 分钟'),
-                );
-              }).toList(),
-              onChanged: (int? newValue) {
-                setState(() {
-                  selectedDuration = newValue;
-                });
-              },
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              initialValue: standardPrice.toStringAsFixed(2),
-              readOnly: true,
-              decoration: const InputDecoration(
-                labelText: '标准价格',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              controller: adjustedPriceController,
-              decoration: const InputDecoration(
-                labelText: '调整价格',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              child: const Text('保存'),
-              onPressed: saveData,
-            ),
-          ],
-        ),
+            );
+          }
+        },
       ),
     );
   }
@@ -218,14 +244,37 @@ class _StudentDocumentEditPageState extends State<StudentDocumentEditPage> {
       return;
     }
 
+    double? adjustedPriceValue = double.tryParse(adjustedPriceController.text);
+
+    if (adjustedPriceValue == null || adjustedPriceValue < 0) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('输入错误'),
+            content: const Text('请输入有效的调整价格'),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('确定'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+
     Map<String, dynamic> data = {
       'stuId': widget.stuId,
       'subjectId': widget.subjectId,
       'subjectSubId': widget.subjectSubId,
       'adjustedDate': widget.adjustedDate,
-      'payStyle': isMonthlyPayment ? 1 : 0,
+      'payStyle': payStyle,
       'duration': selectedDuration,
-      'lessonFeeAdjusted': double.tryParse(adjustedPriceController.text) ?? 0.0,
+      'lessonFee': standardPrice,
+      'lessonFeeAdjusted': adjustedPriceValue,
+      'minutesPerLsn':selectedDuration,
     };
 
     try {
