fb104fff0544895187f487fcd017f72f2e9ab4ca
JesusisLove <liu_ym@hotmail.com>
2024-07-20 10:02:27 +0900

    2024/07/20 后端开发：固定排课周次Batch管理表作成 1.生成一年一度的52或53周的batch执行管理表。 2.周次排课batch执行画面作成。 	2-1.画面初期化状态，排课按钮可用（按钮蓝色显示），撤销按钮不可用（按钮灰色显示） 3.点击某周的排课按钮，执行周次排课（纯粹地执行了数据库的存储过程：sp_weekly_batch_lsn_schedule_process） 	3-1.排课按钮不可用（按钮灰色显示），撤销按钮可用（按钮红色显示） 4.点击撤销按钮，执行该周次排课的撤销 		4-1.排课按钮可用（按钮蓝色显示），撤销按钮不可用（按钮灰色显示） 5.点击撤销按钮前做以下Check， 	5-1.排课中，只要有一节课，在课程表里执行了签到，这批排课就撤销不可。

--- 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/controller/Kn05S002WeekCalculatorController.java
+++ 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/controller/Kn05S002WeekCalculatorController.java
@@ -5,8 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -20,11 +22,22 @@ import java.util.List;
 public class Kn05S002WeekCalculatorController {
     @Autowired
     private Kn05S002WeekCalculatorDao kn05S002WeekCalculatorDao;
-    // 【KNPiano后台维护 学生信息】ボタンをクリック
+    // 【KNPiano后台维护 固定排课周次管理表】ボタンをクリック
     @GetMapping("/kn_calculate_Weeks")
     public String list(Model model) {
         List<Kn05S002FixedLsnStatusBean> collection = kn05S002WeekCalculatorDao.getInfoList();
-        model.addAttribute("infoList",collection);
+        model.addAttribute("infoList", collection);
+        // 不知道为什么这部分代码没有问题的，但是页面就是显示不出消息来。待查
+        // if (model.containsAttribute("strMessage")) {
+        //     Object strMessageObj = model.getAttribute("strMessage");
+        //     String strMessage = (strMessageObj != null) ? strMessageObj.toString() : "null";
+        //     model.addAttribute("strMessage", strMessage);
+        // }
+
+        // 用弹出提示框的方式来取代上面处理,还是显示不出消息来
+        if (model.containsAttribute("apiResponse")) {
+            model.addAttribute("apiResponse", model.getAttribute("apiResponse"));
+        }
         return "kn_calculate_Weeks/kncalculateweeks_list";
     }
 
@@ -39,7 +52,6 @@ public class Kn05S002WeekCalculatorController {
     @ResponseBody
     public ResponseEntity<?> deleteAllWeeks() {
         try {
-            
             /*
             // 方法一
             // 如果你想要在响应中包含一些信息，你可以这样修改：这会返回一个状态码为 200 的响应，响应体为文本 "All weeks deleted successfully"。
@@ -55,5 +67,69 @@ public class Kn05S002WeekCalculatorController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting all weeks");
         }
+    }
+
+    // 执行一周的固定排课计划
+    @GetMapping("/kn_excute_Week_lsn_schedual/{weekStart}/{weekEnd}")
+    public String excuteLsnWeeklySchedual(@PathVariable String weekStart,
+                                          @PathVariable String weekEnd) {
+        kn05S002WeekCalculatorDao.excuteLsnWeeklySchedual(weekStart,weekEnd);
+        return "redirect:/kn_calculate_Weeks";
+    }
+
+    // 撤销一周的固定排课计划
+    @GetMapping("/kn_cancel_Week_lsn_schedual/{weekStart}/{weekEnd}")
+    public String cancelLsnWeeklySchedual(@PathVariable String weekStart,
+                                          @PathVariable String weekEnd,
+                                          RedirectAttributes redirectAttributes) {
+        boolean blnProcess = kn05S002WeekCalculatorDao.cancelLsnWeeklySchedual(weekStart,weekEnd);
+        ApiResponse apiResponse = new ApiResponse();
+        // redirectAttributes.addFlashAttribute("strMessage", strMessage);
+
+        if (blnProcess) {
+            apiResponse.setSuccess(true);
+            apiResponse.setMsgCode("CANCEL_SUCCESS");
+            apiResponse.setMsgContent("撤销完成。");
+        } else {
+            apiResponse.setSuccess(false);
+            apiResponse.setMsgCode("CANCEL_FAILURE");
+            apiResponse.setMsgContent("因为已有签到排课，这批排课不能撤销。");
+        }
+        // 将 ApiResponse 对象存储在 RedirectAttributes 中
+        redirectAttributes.addFlashAttribute("apiResponse", apiResponse);
+        return "redirect:/kn_calculate_Weeks";
+    }
+
+}
+
+ class ApiResponse {
+    private boolean success;
+    private String msgCode;
+    private String msgContent;
+
+    // Getter 方法
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMsgCode() {
+        return msgCode;
+    }
+
+    public String getMsgContent() {
+        return msgContent;
+    }
+
+    // Setter 方法
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public void setMsgCode(String msgCode) {
+        this.msgCode = msgCode;
+    }
+
+    public void setMsgContent(String msgContent) {
+        this.msgContent = msgContent;
     }
 }
--- 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/dao/Kn05S002WeekCalculatorDao.java
+++ 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/dao/Kn05S002WeekCalculatorDao.java
@@ -3,6 +3,9 @@ package com.liu.springboot04web.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.WeekFields;
@@ -11,6 +14,7 @@ import java.util.Locale;
 
 import com.liu.springboot04web.bean.Kn05S002FixedLsnStatusBean;
 import com.liu.springboot04web.bean.KnPianoBean;
+import com.liu.springboot04web.constant.KNConstant;
 import com.liu.springboot04web.mapper.Kn05S002WeekCalculatorMapper;
 
 @Repository
@@ -55,8 +59,41 @@ public class Kn05S002WeekCalculatorDao implements InterfaceKnPianoDao {
         weekCalculatorMapper.deleteAll();
     }
 
+    // 执行一周的固定排课计划
+    public void excuteLsnWeeklySchedual(@RequestParam String weekStart,
+                                        @RequestParam String weekEnd) {
+            // 开始一周的排课
+            weekCalculatorMapper.doLsnWeeklySchedual(weekStart, weekEnd, KNConstant.CONSTANT_KN_LSN_SEQ);
+
+            // 更新年度batch周次表的排课状态（fixed_status:0表示该周的batch排课未执行，1表示该周的batch排课完了）
+            weekCalculatorMapper.updateWeeklyBatchStatus(weekStart, weekEnd, 1);
+    }
+
+    // 撤销一周的固定排课计划
+    public boolean cancelLsnWeeklySchedual(@PathVariable String weekStart,
+                                            @PathVariable String weekEnd) {
+        boolean blnstatus;                                       
+        if (isCancelbale(weekStart, weekEnd)) {
+            // 一旦该星期的学生有但凡有一节课已经签到，那么整个星期的排课撤销操作执行不可🙅‍♂️
+            weekCalculatorMapper.updateWeeklyBatchStatus(weekStart,weekEnd, 0);
+
+            // 删除课程表里该当日期范围的排课信息
+            weekCalculatorMapper.cancelWeeklySchedual(weekStart, weekEnd);
+            blnstatus = true;
+        } else {
+            blnstatus = false;
+        }
+       
+        return blnstatus;
+    }
+
     @Override
     public KnPianoBean getInfoById(String id) {
         throw new UnsupportedOperationException("Unimplemented method 'getInfoById'");
+    }
+
+    // 看课程表（t_info_lesson）里有没有已经签到了的记录如果有，返回false，表示不可以撤销；如果没有，返回true，表示可以撤销
+    private boolean isCancelbale(String startDate, String endDate) {
+        return weekCalculatorMapper.checkLsnHasSignedOrNot(startDate,endDate).size() == 0;
     }
 }
