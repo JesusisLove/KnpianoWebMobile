7c30a584283becb14371ad142148c5b5935838a7
JesusisLove <liu_ym@hotmail.com>
2024-07-27 17:54:34 +0900

    2024/07/26 往数据库里追加一个新表《课费预支付》表（t_info_lsn_fee_advc_pay）

--- 02DBTableGenerateTool/knpiano-db-table-generator-jpa/src/main/java/com/liu/springboot06datajpa/SpringBoot06DataJpaApplication.java
+++ 02DBTableGenerateTool/knpiano-db-table-generator-jpa/src/main/java/com/liu/springboot06datajpa/SpringBoot06DataJpaApplication.java
@@ -2,8 +2,10 @@ package com.liu.springboot06datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.liu.springboot06datajpa.db_table.table_entity")
 public class SpringBoot06DataJpaApplication {
 
     public static void main(String[] args) {
