e300c99b967aaf6ff5e7767560240697fbedc4cd
JesusisLove <liu_ym@hotmail.com>
2024-07-31 23:49:17 +0900

    2024/07/31 后端开发，数据库开发：课费预支付Bug对应，排课日期值的时分值丢失【yyyy-MM-dd 00:00】，。 1.Bug现象，课费预支付的Web页面，选中预支付科目后，点击“课费预支付”按钮   页面上的排课计划日期，比如【2024-08-01 13:30】，在执行了“sp_execute_advc_lsn_fee_pay”存储过程之后，发现 	插入到课程表（t_info_lesson）的排课计划日期变成了【2024-08-01 00:00】，时分值缺失。 2.调查确认：   页面的【2024-08-01 13:30】，映射到Bean的Date的日期变量schedualDate没有问题。   问题出在了 Java 到 MySQL 的数据传输过程中日期类型的不协调导致时分值缺失。
    ３.解决对策是，Kn02F003LsnFeeAdvcPayMapper.xml里
        把【jdbcType=DATE】变成【jdbcType=TIMESTAMP】
        👉即，#{schedualDate,  mode=IN, jdbcType=TIMESTAMP},问题解决了。

--- 02DBTableGenerateTool/MySQL_KNStudent/SQL文/调试调用预支付存储过程.sql
+++ 02DBTableGenerateTool/MySQL_KNStudent/SQL文/调试调用预支付存储过程.sql
@@ -1,11 +1,4 @@
--- 建立调用课费预支付存储过程日志表
--- CREATE TABLE sp_execution_log (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     procedure_name VARCHAR(100),
---     step_name VARCHAR(100),
---     result VARCHAR(255),
---     execution_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
+
 
 -- 调试调用执行课费预支付存储过程
 CALL sp_execute_advc_lsn_fee_pay(
