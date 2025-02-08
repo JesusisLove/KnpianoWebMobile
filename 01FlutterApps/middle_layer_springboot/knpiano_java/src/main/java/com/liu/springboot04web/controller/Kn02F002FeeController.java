060c752e4a8984b169374380d69f1e2ce45694c7
JesusisLove <liu_ym@hotmail.com>
2025-02-08 22:34:55 +0900

    2025/02/08 ①后端Web端：给明细一览的Tab卡片上的学生姓名按照从A到Z的顺序排列整理

--- 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/controller/Kn02F002FeeController.java
+++ 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/controller/Kn02F002FeeController.java
@@ -15,10 +15,10 @@ import java.time.Year;
 import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.stream.Collectors;
 @Controller
 public class Kn02F002FeeController{
     final List<String> knYear; 
@@ -157,18 +157,23 @@ public class Kn02F002FeeController{
 
     // 从结果集中去除掉重复的星期，前端页面脚本以此定义tab名
     private Map<String, String> getResultsTabStus(Collection<Kn02F002FeeBean> collection) {
-
-        Map<String, String> activeStudentsMap = new HashMap<>();
-        Set<String> seenStuIds = new HashSet<>();
-
+        // 首先创建一个用于去重的Map
+        Map<String, String> tempMap = new HashMap<>();
+        
+        // 填充临时Map
         for (Kn02F002FeeBean bean : collection) {
-            String stuId = bean.getStuId();
-            if (!seenStuIds.contains(stuId)) {
-                activeStudentsMap.put(stuId, bean.getStuName());
-                seenStuIds.add(stuId);
-            }
+            tempMap.putIfAbsent(bean.getStuId(), bean.getStuName());
         }
-
-        return activeStudentsMap;
+        
+        // 按值（学生姓名）排序并收集到新的LinkedHashMap中
+        return tempMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(
+                    Map.Entry::getKey,
+                    Map.Entry::getValue,
+                    (oldValue, newValue) -> oldValue,  // 处理重复键的情况
+                    LinkedHashMap::new  // 使用LinkedHashMap保持排序
+                ));
     }
 }
