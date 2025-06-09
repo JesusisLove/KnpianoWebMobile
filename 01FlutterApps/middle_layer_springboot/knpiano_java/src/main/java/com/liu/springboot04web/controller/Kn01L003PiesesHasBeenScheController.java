8e977c1475a45aa2471b97e62b2c74df70762e6c
JesusisLove <liu_ym@hotmail.com>
2025-06-10 00:22:15 +0900

    2025/06/10 🔳MacStudio Web端 ❶:碎课拼成整课并转换成正课，❷:零碎加课已经转换正课一览和撤销正课还原到零碎加课功能

---
+++ 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/controller/Kn01L003PiesesHasBeenScheController.java
@@ -0,0 +1,192 @@
+package com.liu.springboot04web.controller;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.liu.springboot04web.bean.Kn01L003ExtraPicesesBean;
+import com.liu.springboot04web.dao.Kn01L003PiesesHasBeenScheDao;
+import com.liu.springboot04web.othercommon.DateUtils;
+import com.liu.springboot04web.service.ComboListInfoService;
+
+@Controller
+@Service
+public class Kn01L003PiesesHasBeenScheController {
+
+    final List<String> knYear;
+    final List<String> knMonth;
+
+    // 把要付费的学生信息拿到前台画面，给学生下拉列表框做初期化
+    Collection<Kn01L003ExtraPicesesBean> extra2ScheStuList;
+    @Autowired
+    private Kn01L003PiesesHasBeenScheDao kn01L003PiesesHasBeenScheDao;
+
+
+    public Kn01L003PiesesHasBeenScheController(ComboListInfoService combListInfo) {
+        // 初期化年度下拉列表框
+        this.knYear = DateUtils.getYearList();
+
+        // 初期化月份下拉列表框
+        this.knMonth = combListInfo.getMonths();
+        this.extra2ScheStuList = null;
+    }
+
+
+    // 页面初期化
+    @GetMapping("/kn_pieses_hasbeen_sche")
+    public String list(Model model) {
+
+        // 画面检索条件保持变量初始化前端检索部
+        LocalDate currentDate = LocalDate.now();// 获取当前日期
+
+        // 格式化为 yyyy
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy");
+        String year = currentDate.format(formatter);
+        Collection<Kn01L003ExtraPicesesBean> collectionNew = kn01L003PiesesHasBeenScheDao.getPicesesHasBeenScheLsnList(year,
+                null);
+        extra2ScheStuList = collectionNew;
+        model.addAttribute("infoListPiceseHasBeenSche", collectionNew);
+
+        Collection<Kn01L003ExtraPicesesBean> collectionOld = kn01L003PiesesHasBeenScheDao.getExtraPicesesIntoOneLsnList(year,
+                null);
+        model.addAttribute("infoListPicesExtra", collectionOld);
+
+        // TODO 还要看课费支付表，对于已经结算完了的课，就让撤销按钮不可用。（课费结算完了，不能撤销）
+
+
+        /* 初始化画面检索部 */
+        // 把要消化加课的学生信息拿到前台画面，给学生下拉列表框做初期化
+        model.addAttribute("extra2ScheStuList", extra2ScheStuList);
+
+        // 利用resultsTabStus的学生名，在前端页面做Tab
+        Map<String, String> resultsTabStus = getResultsTabStus(collectionNew);
+        model.addAttribute("resultsTabStus", resultsTabStus);
+
+        // 年度下拉列表框初期化前台页面
+        model.addAttribute("knyearlist", knYear);
+
+        return "kn_lsn_extra_pis_hsbn_sche/kn_pis_hsbn_sche_list";
+    }
+
+
+    // 选择年度切换，初始化该年度下的学生姓名下拉列表框
+    @GetMapping("/kn_pieses_hasbeen_sche/{year}")
+    public String list(@PathVariable("year") String year, Model model) {
+        List<Kn01L003ExtraPicesesBean> list = kn01L003PiesesHasBeenScheDao.getPicesesHasBeenScheLsnList(year,null);
+        model.addAttribute("extra2ScheStuList", list);
+
+        // 创建 Map 并添加到 Model
+        Map<String, String> extra2ScheMap = new HashMap<>();
+        extra2ScheMap.put("selectedyear", year);
+        model.addAttribute("extra2ScheMap", extra2ScheMap);
+
+        model.addAttribute("knyearlist", knYear);
+        model.addAttribute("knmonthlist", knMonth);
+
+        return "kn_lsn_extra_pis_hsbn_sche/kn_pis_hsbn_sche_list";
+    }
+
+
+    // 检索该年度，该学生的零碎课数据
+    @GetMapping("/kn_pieses_hasbeen_sche/search")
+    public String search(@RequestParam Map<String, Object> queryParams, Model model) {
+        String lsnYear = (String) queryParams.get("selectedyear");
+        String stuId = (String) queryParams.get("stuId");
+
+        Collection<Kn01L003ExtraPicesesBean> collectionNew = kn01L003PiesesHasBeenScheDao.getPicesesHasBeenScheLsnList(lsnYear,
+                stuId);
+        model.addAttribute("infoListPiceseHasBeenSche", collectionNew);
+
+        Collection<Kn01L003ExtraPicesesBean> collectionOld = kn01L003PiesesHasBeenScheDao.getExtraPicesesIntoOneLsnList(lsnYear,
+                stuId);
+        model.addAttribute("infoListPicesExtra", collectionOld);
+
+        // 回传参数设置（画面检索部的查询参数）
+        Map<String, Object> backForwordMap = new HashMap<>();
+        backForwordMap.putAll(queryParams);
+
+        model.addAttribute("extra2ScheMap", backForwordMap);
+        model.addAttribute("currentyear", lsnYear);
+        model.addAttribute("knyearlist", knYear);
+
+         // 另外，pullDownlist结果集里还有科目和该科目一节课的课程时长的信息。用于碎课拼整课的计算和判断。
+        model.addAttribute("extra2ScheStuList", extra2ScheStuList);
+
+        // 利用resultsTabStus的学生名，在前端页面做Tab
+        Map<String, String> resultsTabStus = getResultsTabStus(collectionNew);
+        model.addAttribute("resultsTabStus", resultsTabStus);
+
+        return "kn_lsn_extra_pis_hsbn_sche/kn_pis_hsbn_sche_list";
+    }
+    
+
+    @PostMapping("/kn_revoke_assembled_lesson")
+    @Transactional(rollbackFor = Exception.class)
+    public String undoAssembledLesson(@RequestParam Map<String, Object> queryParams, Model model) {
+        // String year = (String) queryParams.get("selectedyear");
+        // String stuId = (String) queryParams.get("stuId");
+        String lessonId = (String) queryParams.get("lessonId");
+
+        try {
+
+            // 从零碎课拼凑整课转换正课中间表，取出零碎加课的课程编号（old_lesson_id)
+            List<String> lsnIdsList = kn01L003PiesesHasBeenScheDao.getOldLessonIds(lessonId);
+            for (String oldLsnId : lsnIdsList) {
+                // 更新课程表零碎课的逻辑删除flg（set del_flg = 0)，恢复成零碎课的状态：更新条件： old_lesson_id
+                kn01L003PiesesHasBeenScheDao.undoLogicalDelLesson(oldLsnId);
+                kn01L003PiesesHasBeenScheDao.undoLogicalDelLsfFee(oldLsnId);
+            }
+
+            kn01L003PiesesHasBeenScheDao.deletePiesHsbnScheFee(lessonId);
+
+            // 物理删除课程表掉拼凑整课转正课的课程记录。删除条件：lesson_id
+            kn01L003PiesesHasBeenScheDao.deletePiesHsbnSche(lessonId);
+
+            // 物理删除整课转正课中间表的课程关联记录：删除条件：lesson_id
+            kn01L003PiesesHasBeenScheDao.deletePiceseExtraIntoOne(lessonId);
+
+        } catch (Exception e)  {
+            e.printStackTrace();
+        }
+        
+        return "redirect:/kn_pieses_hasbeen_sche";
+    }
+
+
+    // 从结果集中去除掉重复的星期，前端页面脚本以此定义tab名
+    private Map<String, String> getResultsTabStus(Collection<Kn01L003ExtraPicesesBean> collection) {
+        // 首先创建一个用于去重的Map
+        Map<String, String> tempMap = new HashMap<>();
+
+        // 填充临时Map
+        for (Kn01L003ExtraPicesesBean bean : collection) {
+            tempMap.putIfAbsent(bean.getStuId(), bean.getStuName());
+        }
+
+        // 按值（学生姓名）排序并收集到新的LinkedHashMap中
+        return tempMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, // 处理重复键的情况
+                        LinkedHashMap::new // 使用LinkedHashMap保持排序
+                ));
+    }
+
+}
--- 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/dao/Kn01L003ExtraPiesesIntoOneDao.java
+++ 01FlutterApps/middle_layer_springboot/knpiano_java/src/main/java/com/liu/springboot04web/dao/Kn01L003ExtraPiesesIntoOneDao.java
@@ -1,27 +1,142 @@
 package com.liu.springboot04web.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.liu.springboot04web.bean.Kn01L002ExtraToScheBean;
+import com.liu.springboot04web.bean.Kn01L002LsnBean;
+import com.liu.springboot04web.bean.Kn01L003ExtraPicesesBean;
+import com.liu.springboot04web.bean.Kn02F002FeeBean;
+import com.liu.springboot04web.bean.Kn02F004PayBean;
+import com.liu.springboot04web.bean.Kn03D004StuDocBean;
+import com.liu.springboot04web.constant.KNConstant;
+import com.liu.springboot04web.mapper.Kn01L002LsnMapper;
 import com.liu.springboot04web.mapper.Kn01L003ExtraPiesesIntoOneMapper;
+import com.liu.springboot04web.othercommon.DateUtils;
 
 @Repository
 public class Kn01L003ExtraPiesesIntoOneDao {
 
     @Autowired
     private Kn01L003ExtraPiesesIntoOneMapper kn01L003ExtraPiesesIntoOneMapper;
+
+    // 对凑成整课的加课转正课后执行签到用
+    @Autowired
+    private Kn01L002LsnMapper knLsn001Mapper;
+    @Autowired
+    private Kn03D004StuDocDao knStuDoc001Dao;
+    @Autowired
+    private Kn02F002FeeDao kn02F002FeeDao;
+    @Autowired
+    private Kn02F004PayDao kn02f004PayDao;
     
-    public List<Kn01L002ExtraToScheBean> getExtraPiesesLsnList(String year, String stuId) {
+    
+    // 画面初期化，检索
+    public List<Kn01L003ExtraPicesesBean> getExtraPiesesLsnList(String year, String stuId) {
         return kn01L003ExtraPiesesIntoOneMapper.getExtraPiesesLsnList(year, stuId);
     }
 
-    public List<Kn01L002ExtraToScheBean> getSearchInfo4Stu(String year) {
+
+    // 年度，学生姓名的联动变化
+    public List<Kn01L003ExtraPicesesBean> getSearchInfo4Stu(String year) {
         return kn01L003ExtraPiesesIntoOneMapper.getSearchInfo4Stu(year);
     }
 
-    
+
+    // 执行拼凑完成的加课换正课
+    public void excutePicesesIntoOneAndChangeToSche(Kn01L002LsnBean knLsn001Bean) {
+        // 课程编号采番
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("parm_in", KNConstant.CONSTANT_KN_LSN_SEQ);
+        // 授業自動採番
+        knLsn001Mapper.getNextSequence(map);
+        knLsn001Bean.setLessonId(KNConstant.CONSTANT_KN_LSN_SEQ + (Integer) map.get("parm_out"));
+
+        // 课程编号の自動採番
+        knLsn001Mapper.getNextSequence(map);
+        knLsn001Mapper.insertInfo(knLsn001Bean);
+    }
+
+
+    // 保存拼成整课的课程ID和零碎课的课程ID的关联映射
+    public int save(String lessonId, String oldLessonId) {
+        return kn01L003ExtraPiesesIntoOneMapper.savePiceseExtraIntoOne(lessonId, oldLessonId);
+    }
+
+    // 逻辑删除《课程表》里零碎课的课程信息（set del_flg = 1）
+    public int logicalDelLesson(String lessonId) {
+
+        return kn01L003ExtraPiesesIntoOneMapper.logicalDelLesson(lessonId);
+    }
+
+    // 逻辑删除《课费表》里零碎课的课程信息（set del_flg = 1）
+    public int logicalDelLsfFee(String lessonId) {
+        return kn01L003ExtraPiesesIntoOneMapper.logicalDelLsfFee(lessonId);
+    }
+
+    // 像计划课签到一样走签到的业务流程
+    public void excuteSign(Kn01L002LsnBean knLsn001Bean) {
+
+        addNewLsnFee(knLsn001Bean);
+
+    }
+
+
+    // 将该签到课程新规登录到《课费管理表》里
+    private void addNewLsnFee(Kn01L002LsnBean knLsn001Bean) {
+        // 该签到课程的课费信息作成处理
+        Kn02F002FeeBean kn02F002FeeBean = setLsnFeeBean(knLsn001Bean);
+
+        // 新规课程费信息，保存到《课费管理表》里
+        kn02F002FeeDao.save(kn02F002FeeBean);
+    }
+
+
+    private Kn02F002FeeBean setLsnFeeBean(Kn01L002LsnBean knLsn001Bean) {
+        // 取得该生最新的档案记录信息
+        Kn03D004StuDocBean stuDocBean = knStuDoc001Dao.getLsnPrice(knLsn001Bean.getStuId(), knLsn001Bean.getSubjectId(),
+                knLsn001Bean.getSubjectSubId());
+
+        Kn02F002FeeBean bean = new Kn02F002FeeBean();
+        bean.setStuId(knLsn001Bean.getStuId());
+        bean.setLessonId(knLsn001Bean.getLessonId());
+        // 有调整价格的使用调整价格
+        bean.setLsnFee(
+                stuDocBean.getLessonFeeAdjusted() > 0 ? stuDocBean.getLessonFeeAdjusted() : stuDocBean.getLessonFee());
+        // 利用计划课取得产生课费的月份
+        bean.setLsnMonth(DateUtils.getCurrentDateYearMonth(knLsn001Bean.getSchedualDate()));
+
+        // 课程类型lessonType 0:按课时结算的课程（课结算） 1:按月结算的课程（月计划） 2:按月结算的课程（月加课）
+        bean.setLessonType(knLsn001Bean.getLessonType());
+        bean.setSubjectId(knLsn001Bean.getSubjectId());
+        bean.setPayStyle(stuDocBean.getPayStyle());
+        bean.setOwnFlg(0);
+
+        // 按月精算的课程的own_flg设定处理
+        // 计划课还分为月计划:(lessonType=1)，和月加课:(lessonType=2)，这里处理的对象是月计划。月加课的费用另结算
+        if (bean.getPayStyle() == 1 && bean.getLessonType() == 1) {
+            setOwnFlg(bean);
+        }
+
+        return bean;
+    }
+
+    /**
+     * 检测对象：月计划课在该月是否未满4节课的情况下做了结算操作
+     * 对象场景：该月的计划课时签到未满（一个月4节课或5节课）的情况下，就执行了月精算处理
+     * （也就是说，该月计划课程还没上满4节课（该月有第五周就时5节课）就已经执行了精算处理）
+     * 业务场景：签到了1节课，就执行了按月计算处理，这样课费表里只有这一节签到课的ownFlg变更为0了
+     * 那么剩下的这3节课，因为是计划课，所以在签到的同时也应当把自身的ownFlg设置为0存到课费金额表里
+     */
+    private void setOwnFlg(Kn02F002FeeBean bean) {
+        List<Kn02F004PayBean> list = kn02f004PayDao.isHasMonthlyPaid(bean.getStuId(), bean.getSubjectId(),
+                bean.getLsnMonth());
+        if (list.size() > 0) {
+            bean.setOwnFlg(1);
+        }
+    }
 
 }
